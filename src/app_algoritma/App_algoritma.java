8aeb8e097c6f3d13ea4e8a1219926aef9889dd24
Df-pro <dwiferdianto69@gmail.com>
2026-06-22 14:33:44 +0700

    tes yang kedua

---
+++ src/app_algoritma/App_algoritma.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package app_algoritma;
+
+/**
+ *
+ * @author dwife
+ */
+
+public class App_algoritma {
+
+    public static void main(String[] args) {
+        String nama = "Dwi Ferdianto";
+        int umur = 21;
+
+        System.out.println("Nama : " + nama);
+        System.out.println("Umur : " + umur + " tahun");
+    }
+}
