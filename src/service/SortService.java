ba0a58135436883efe1d8981147813f1679ba9bf
azkaalasy <azkaalasyari1@gmail.com>
2026-06-25 03:13:39 +0700

    maap cuma bisa bantu dikit, boleh di koreksi dan dibenahi jika ada kurang ataupun salah

--- src/service/SortService.java
+++ src/service/SortService.java
@@ -3,11 +3,23 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package service;
-
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import model.Tersangka;
 /**
  *
  * @author dwife
  */
 public class SortService {
-    
+    public static void sortNama(ArrayList<Tersangka> list) {
+
+        Collections.sort(list, new Comparator<Tersangka>() {
+            @Override
+            public int compare(Tersangka o1, Tersangka o2) {
+                return o1.getNama().compareToIgnoreCase(o2.getNama());
+            }
+        });
+
+    }
 }
