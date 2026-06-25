cb2f0a827e679d6908b0a8f0d34f609e8cd90b9d
azkaalasy <azkaalasyari1@gmail.com>
2026-06-25 21:58:36 +0700

    maap cuma bisa bantu dikit, boleh di koreksi dan dibenahi jika ada kurang ataupun salah

--- src/model/Tersangka.java
+++ src/model/Tersangka.java
@@ -9,5 +9,62 @@ package model;
  * @author dwife
  */
 public class Tersangka {
-    
+    private String idTersangka;
+    private String nama;
+    private int umur;
+    private String alamat;
+    private String jenisKelamin;
+
+    public Tersangka(String idTersangka, String nama, int umur, String alamat, String jenisKelamin) {
+        this.idTersangka = idTersangka;
+        this.nama = nama;
+        this.umur = umur;
+        this.alamat = alamat;
+        this.jenisKelamin = jenisKelamin;
+    }
+
+    public String getIdTersangka() {
+        return idTersangka;
+    }
+
+    public void setIdTersangka(String idTersangka) {
+        this.idTersangka = idTersangka;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public int getUmur() {
+        return umur;
+    }
+
+    public void setUmur(int umur) {
+        this.umur = umur;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    public String getJenisKelamin() {
+        return jenisKelamin;
+    }
+
+    public void setJenisKelamin(String jenisKelamin) {
+        this.jenisKelamin = jenisKelamin;
+    }
+
+    @Override
+    public String toString() {
+        return idTersangka + " - " + nama;
+    }
 }
